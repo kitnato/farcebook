c7312d72ef1d1e38ffb9c70c41d367a7d70c92b7
Chris N <anull@proton.me>
2023-05-11 14:19:28 +0200

    fix: added base directory for gh-pages assets.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
+  base: "/farcebook/",
   plugins: [
     checker({
       eslint: { lintCommand: "eslint './src/**/*.{ts,tsx}'" },
